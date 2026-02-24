928575bed2600619112648aea85723daa36549d0
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-02-25 08:31:34 +0900

    etc

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod icon;
 use icon::*;
 
 fn main() {
-    let icon_info = IconInfo::new(1024.0, "#00000000".to_string(), "#27e2a4".to_string());
+    let icon_info = IconInfo::new(1024.0, "#27e2a4".to_string(), "#121212".to_string());
     let dir = format!("result/{}", icon_info);
     icon::Icon::new(&icon_info).save(dir);
 }
